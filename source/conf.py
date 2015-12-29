d008638c051797a08dd3fe9508ab7c00ecf8d391
RJ Ryan <rryan@mixxx.org>
2015-12-28 22:35:14 -0500

    Disable TODOs for the release branch.

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ extensions = [
     'sphinx.ext.graphviz',
 ]
 
-todo_include_todos = True
+todo_include_todos = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
