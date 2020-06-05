bde9ee5cacd6641febc2a488fa2a3b9d7f0695e5
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-06-05 22:39:11 +0200

    Netlify: Build all manual versions

---
+++ build_manual.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Add remote to build other branches, too
+if git remote -v | grep -q origin
+then
+    git remote remove origin
+fi
+git remote add origin --fetch https://github.com/mixxxdj/manual.git
+git fetch origin
+
+# Parse languages and ensure that "en" is built last
+cd source || exit 1
+LANGUAGES="$(python -c 'import conf; print(" ".join(sorted((lang for lang in conf.supported_languages.keys() if lang != "en"), reverse=True)))')"
+cd .. || exit 1
+LANGUAGES="$LANGUAGES en"
+NUM_LANGUAGES="$(printf '%s' "$LANGUAGES" | wc -w)"
+
+# Write _redirects file
+mkdir -p build/html
+printf '/           /latest/               302\n' > build/html/_redirects
+printf '/latest/*   /2.2/:splat            301\n' >> build/html/_redirects
+
+i=1
+for lang in $LANGUAGES
+do
+    printf -- '----- Building language "%s"... [%d/%d] -----\n' "$lang" "$i" "$NUM_LANGUAGES"
+
+    if [ "$lang" = "en" ]
+    then
+        printf '/:version/* /:version/en/:splat    301\n' >> build/html/_redirects
+    else
+        printf '/:version/* /:version/%s/:splat    301 Language=%s\n' "$lang" "$lang" >> build/html/_redirects
+    fi
+    make versionedhtml SPHINXOPTS="-q -j $(nproc) -Dlanguage=$lang"
+    i=$((i + 1))
+done
