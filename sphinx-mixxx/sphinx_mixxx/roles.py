5f7cbb9423e6fd1ab35e7736574438038b0a81ff
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2020-10-30 17:00:38 +0100

    sphinx-mixxx: Fix usage of ampersand in hwlabel role

--- sphinx-mixxx/sphinx_mixxx/roles.py
+++ sphinx-mixxx/sphinx_mixxx/roles.py
@@ -1,8 +1,16 @@
-from sphinx.roles import GUILabel
+from typing import List, Tuple
+
+from docutils import nodes
+from sphinx.roles import SphinxRole
 
 
-class HWLabel(GUILabel):
+class HWLabel(SphinxRole):
     """Label on external hardware (such as controllers)."""
+
+    def run(self) -> Tuple[List[nodes.Node], List[nodes.system_message]]:
+        node = nodes.inline(rawtext=self.rawtext, classes=[self.name])
+        node += nodes.Text(self.text)
+        return [node], []
 
 
 specific_docroles = {
