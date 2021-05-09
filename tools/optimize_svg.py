bfa9ca652fc52125e7c35709fe4c522b95a60589
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2021-05-10 00:18:05 +0200

    tools/optimize_svg.py: Add multiprocessing support

--- tools/optimize_svg.py
+++ tools/optimize_svg.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python3
 import argparse
+import concurrent.futures
+import multiprocessing
 import tempfile
 import shutil
 from scour import scour
 
-ARGS = (
-    "--enable-comment-stripping",
-    "--enable-id-stripping",
-    "--enable-viewboxing",
-    "--indent=none",
-    "--no-line-breaks",
-    "--remove-descriptions",
-    "--remove-metadata",
-    "--remove-titles",
+OPTIONS = scour.parse_args(
+    [
+        "--enable-comment-stripping",
+        "--enable-id-stripping",
+        "--enable-viewboxing",
+        "--indent=none",
+        "--no-line-breaks",
+        "--remove-descriptions",
+        "--remove-metadata",
+        "--remove-titles",
+    ]
 )
+
+
+def optimize_file(input_file):
+    with open(input_file, mode="rb") as input:
+        with tempfile.NamedTemporaryFile(
+            suffix=".svg", mode="wb", delete=False
+        ) as output:
+            scour.start(OPTIONS, input, output)
+    shutil.move(output.name, input_file)
 
 
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("input_file", nargs="+")
+    parser.add_argument(
+        "-j", "--jobs", type=int, default=multiprocessing.cpu_count()
+    )
     args = parser.parse_args(argv)
-    options = scour.parse_args(list(ARGS))
-
-    for input_file in args.input_file:
-        with open(input_file, mode="rb") as input:
-            with tempfile.NamedTemporaryFile(
-                suffix=".svg", mode="wb", delete=False
-            ) as output:
-                scour.start(options, input, output)
-        shutil.move(output.name, input_file)
+    max_workers = args.jobs
+    print(f"Using {max_workers} concurrent jobs.")
+    with concurrent.futures.ProcessPoolExecutor(max_workers) as executor:
+        executor.map(optimize_file, args.input_file)
 
 
 if __name__ == "__main__":
