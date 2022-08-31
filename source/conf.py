3fa07cc15340eb5a6b26cec9c8a0a8c5d370e342
Daniel Schürmann <daschuer@mixxx.org>
2022-08-31 10:54:25 +0200

    No Linkcheck for https://www.roland.com/global/products/dj-505/*

--- source/conf.py
+++ source/conf.py
@@ -452,7 +452,7 @@ linkcheck_ignore = [
     "https://www.numark.com/images/product_downloads/MixtrackPro3-UserGuide-v1.1.pdf",
     "https://www.numark.com/product/*",
     "https://www.pioneerdj.com/en/support/software/controller/*",
-    "https://www.roland.com/global/products/dj-505/downloads",
+    "https://www.roland.com/global/products/dj-505/*",
 ]
 
 # Avoid freezing during linkcheck
